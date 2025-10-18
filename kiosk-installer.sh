7a9b989253eda17e91a5832374e8ad901a66a3aa
Zango <ace@acewolf.de>
2025-10-18 16:45:21 +0200

    Update kiosk-installer.sh

--- kiosk-installer.sh
+++ kiosk-installer.sh
@@ -1,29 +1,59 @@
 #!/bin/bash
 
-# be new
-apt-get update
+# THE KIOSK URL
+KIOSK_URL="https://example.com/"
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+
+
+if [ "$EUID" -ne 0 ]
+        then echo -e "${RED}Please run as root${NC}"
+        exit
+fi
+
 
 # get software
-apt-get install \
-    unclutter \
-    xorg \
-    chromium \
-    openbox \
-    lightdm \
-    locales \
-    -y
+echo -e "${GREEN}Installing required packages via apt...${NC}"
+cmdout=$(apt-get update && apt-get install \
+                unclutter \
+                xorg \
+                firefox-esr \
+                openbox \
+                lightdm \
+                locales \
+                zenity \
+                psmisc \
+                -y 2>&1)
+es=$?
+if ((es)); then
+        echo -e >&2 "${RED}Error installing packages ($es):${NC}\n${ORANGE}$cmdout${NC}"
+        echo -e "${RED}ABORTING KIOSK INSTALL!${NC}"
+        exit 1
+else
+        echo -e "${GREEN}Install of packages successful.${NC}"
+fi
+
+
+echo -e "${GREEN}Creating users and groups...${NC}"
 
 # dir
 mkdir -p /home/kiosk/.config/openbox
 
 # create group
-groupadd -f kiosk
+getent group kiosk &>/dev/null || groupadd kiosk
 
 # create user if not exists
-id -u kiosk &>/dev/null || useradd -m kiosk -g kiosk -s /bin/bash 
+id -u kiosk &>/dev/null || useradd -m kiosk -g kiosk -s /bin/bash
 
 # rights
 chown -R kiosk:kiosk /home/kiosk
+
+
+echo -e "${GREEN}Writing configs...${NC}"
 
 # remove virtual consoles
 if [ -e "/etc/X11/xorg.conf" ]; then
@@ -41,9 +71,102 @@ if [ -e "/etc/lightdm/lightdm.conf" ]; then
 fi
 cat > /etc/lightdm/lightdm.conf << EOF
 [Seat:*]
-xserver-command=X -nocursor -nolisten tcp
+#xserver-command=X -nocursor -nolisten tcp
+xserver-command=X -s 0 dpms
 autologin-user=kiosk
 autologin-session=openbox
+EOF
+
+# create firefox policies
+mkdir -p /etc/firefox
+mkdir -p /etc/firefox/policies
+if [ -e "/etc/firefox/policies/policies.json" ]; then
+  mv /etc/firefox/policies/policies.json /etc/firefox/policies/policies.json.backup
+fi
+cat > /etc/firefox/policies/policies.json << EOF
+{
+        "policies": {
+                "AllowFileSelectionDialogs": false,
+                "AppAutoUpdate": false,
+                "AutofillAddressEnabled": false,
+                "AutofillCreditCardEnabled": false,
+                "BlockAboutAddons": true,
+                "BlockAboutConfig": true,
+                "BlockAboutProfiles": true,
+                "BlockAboutSupport": true,
+                "DisableAppUpdate": true,
+                "DisableDeveloperTools": true,
+                "DisableFeedbackCommands": true,
+                "DisableFirefoxAccounts": true,
+                "DisableFirefoxScreenshots": true,
+                "DisableFirefoxStudies": true,
+                "DisableFormHistory": true,
+                "DisableMasterPasswordCreation": true,
+                "DisablePasswordReveal": true,
+                "DisablePocket": true,
+                "DisableProfileImport": true,
+                "DisableProfileRefresh": true,
+                "DisableSafeMode": true,
+                "DisableSetDesktopBackground": true,
+                "DisableSystemAddonUpdate": true,
+                "DisableTelemetry": true,
+                "DisableThirdPartyModuleBlocking": true,
+                "DisplayBookmarksToolbar": true,
+                "DisplayMenuBar": false,
+                "DontCheckDefaultBrowser": true,
+                "EnableTrackingProtection": {
+                        "Value": true,
+                        "Locked": true
+                },
+                "FirefoxSuggest": {
+                        "WebSuggestions": false,
+                        "SponsoredSuggestions": false,
+                        "ImproveSuggest": false,
+                        "Locked": true
+                },
+                "GenerativeAI": {
+                        "Chatbot": false,
+                        "LinkPreviews": false,
+                        "TabGroups": false,
+                        "Locked": true
+                },
+                "Homepage": {
+                        "URL": "$KIOSK_URL",
+                        "Locked": true,
+                        "StartPage": "homepage-locked"
+                },
+                "ManualAppUpdateOnly": true,
+                "NewTabPage": false,
+                "NoDefaultBookmarks": true,
+                "OfferToSaveLogins": false,
+                "PasswordManagerEnabled": false,
+                "PictureInPicture": {
+                        "Enabled": false,
+                        "Locked": true
+                },
+                "PopupBlocking": {
+                        "Default": true,
+                        "Locked": true
+                },
+                "PrintingEnabled": false,
+                "PromptForDownloadLocation": false,
+                "SanitizeOnShutdown": true,
+                "SearchBar": "separate",
+                "SearchSuggestEnabled": false,
+                "ShowHomeButton": false,
+                "SkipTermsOfUse": true,
+                "TranslateEnabled": false,
+                "UserMessaging": {
+                        "ExtensionRecommendations": false,
+                        "FeatureRecommendations": false,
+                        "UrlbarInterventions": false,
+                        "SkipOnboarding": true,
+                        "MoreFromMozilla": false,
+                        "FirefoxLabs": false,
+                        "Locked": true
+                }
+        }
+}
 EOF
 
 # create autostart
@@ -53,28 +176,29 @@ fi
 cat > /home/kiosk/.config/openbox/autostart << EOF
 #!/bin/bash
 
-KIOSK_URL="https://neave.tv/"
+unclutter -idle 0 -jitter 2000 &
 
-unclutter -idle 0.1 -grab -root &
+# Wait for Network
+zenity --title="Connecting..." --ok-label="" --width=500 --height=100 --info --no-wrap --no-markup --text="System is waiting for an internet connection" &
+while ! ping -c 1 -W 1 8.8.8.8; do
+        echo "Waiting for internet connection...."
+        sleep 1
+done
+killall zenity &
 
+# Start Kiosk
 while :
 do
   xrandr --auto
-  chromium \
-    --noerrdialogs \
-    --no-memcheck \
-    --no-first-run \
-    --start-maximized \
-    --disable \
-    --disable-translate \
-    --disable-infobars \
-    --disable-suggestions-service \
-    --disable-save-password-bubble \
-    --disable-session-crashed-bubble \
-    --incognito \
-    --kiosk $KIOSK_URL
+  firefox-esr --kiosk --private-window --disable-pinch $KIOSK_URL
   sleep 5
 done &
 EOF
 
-echo "Done!"
+
+echo -e "${GREEN}Done!${NC}"
+echo -e "${ORANGE}Rebooting in 30s, press ctrl+c to abort reboot${NC}"
+
+sleep 30
+
+reboot now
